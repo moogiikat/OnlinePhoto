2205da5e2cfaa5a7f73a68af2d2808547822b3c6
moogiikat <monkhoo1997ts@gmail.com>
2021-05-04 12:19:27 +0800

    second commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import './App.css';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/services' component={Services} />
+          <Route path='/products' component={Products} />
+          <Route path='/sign-up' component={SignUp} />
+        </Switch>
+      </Router>
+    </>
+  );
+}
+
+export default App;
+
+// import React, { Component } from 'react';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>Hello World!</h1>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
